06012fc00fb4ec0faf7c7ac9a1f0a59be2daf485
caovicto <caovicto@msu.edu>
2020-08-05 15:57:53 -0400

    edit to top button

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -6,16 +6,10 @@ $(document).ready(function() {
 
 var btn = $('#top-button');
 
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-
 btn.on('click', function(e) {
   e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
+  $('html, body').animate({
+    scrollTop: $("#comic").offset().top
+    });
 });
 
