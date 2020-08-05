0c1ad414063899a6eb760045130d3ed97fc56210
caovicto <caovicto@msu.edu>
2020-08-05 17:06:14 -0400

    fix pop up

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -8,17 +8,3 @@ function topFunction() {
 
 $(document).ready(topFunction); 
 
-//Get the button
-var mybutton = document.getElementById("top-button");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
