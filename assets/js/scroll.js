fff53b8929218ac91f7fcadb31fb9cd59e63519b
caovicto <caovicto@msu.edu>
2020-08-05 16:57:36 -0400

    fix pop up

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,15 +1,24 @@
-$(document).ready(function() { 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
     $('html, body').animate({
-        scrollTop: $("#first-nav").offset().top
+        scrollTop: $("#comic").offset().top
     });
-}); 
+}
 
-var btn = $('#top-button');
 
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({
-    scrollTop: $("#first-nav").offset().top
-    });
-});
+$(document).ready(topFunction); 
+
+//Get the button
+var mybutton = document.getElementById("top-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
